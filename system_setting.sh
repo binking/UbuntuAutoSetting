8c752fe0eb1f095257890be440c15c174d00ba33
binking <jiangzhibin2014.xujie@gmail.com>
2016-10-22 08:26:28 +0800

    init direct

---
+++ system_setting.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/sh
+
+# First, buid dev env
+sudo apt-get install git
+print 'Installed Git succeeded'
+
+sudo apt-get install terminator
+print 'Installed Terminator succeeded'
+
+sudo apt-get install 
