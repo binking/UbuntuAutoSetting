6ebb59f4221f3abba624183943f63ac864bbb62e
Zhibin Jiang <zhibin.jiang@ihandysoft.com>
2018-12-19 17:23:45 +0800

    remove old files

--- build_python_mlenv.sh
+++
@@ -1,8 +0,0 @@
-# Build Graphlab Env, using Py2
-# virtualenv -p python ml_venv
-# source ./bin/activate
-./bin/python -m pip install jupyter
-./bin/python -m pip install --no-cache-dir graphlab-create
-./bin/python -m pip install scipy
-./bin/python -m pip install ski-learn
-./bin/python -m pip install matplotlib
