0d39d1018e14b4d11f36599827733d98c4462f23
Zhibin Jiang <zhibin.jiang@ihandysoft.com>
2019-08-05 11:34:10 +0800

    update mac zsh rc

--- mac/mac_installation.sh
+++ mac/mac_installation.sh
@@ -1,16 +1,31 @@
+# install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install tree
 brew install zsh
-brew install mongodb
-brew install graphviz
-brew cask install java
-brew install mysql-connector-c
-brew install postgresql
+brew install zsh-syntax-highlighting
+export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
+# systen tool
 brew install tmux
-brew install redis
-brew install redis-server
 brew install cmake
 brew install openssl
-brew install npm
 brew install libyaml
 brew install htop
+brew install graphviz
+
+# python dev
+brew install python3
+
+# java dev
+brew cask install java
+
+# database service
+brew install mysql 
+brew install mysql-connector-c
+brew install redis
+brew install redis-server
+brew install mongodb
+brew install postgresql
+
+# node dev
+brew install npm
+
